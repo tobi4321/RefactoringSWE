25ce250194cec5b03d5241c0b113e07ee894fae8
Tobias Bühler <tobias.buehler@t-online.de>
2020-05-13 10:40:47 +0200

    renamed parameter of amountFor method
    added Type definitions to Vector and Enumeration Objects

--- src/Customer.java
+++ src/Customer.java
@@ -1,12 +1,11 @@
 
-import java.lang.*;
 import java.util.*;
 
 class Customer {
     
 	private String name;
     
-	private Vector rentals = new Vector();
+	private Vector<Rental> rentals = new Vector<Rental>();
     
 	public Customer (String newname){
         name = newname;
@@ -25,7 +24,7 @@ class Customer {
     	double totalAmount = 0;
         int frequentRenterPoints = 0;
         
-        Enumeration enum_rentals = rentals.elements();	    
+        Enumeration<Rental> enum_rentals = rentals.elements();	    
         
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
@@ -50,23 +49,23 @@ class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) {
+    private double amountFor(Rental rental) {
         
     	double thisAmount = 0;
         
-    	switch (each.getMovie().getPriceCode()) {
+    	switch (rental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                if (rental.getDaysRented() > 2)
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount += rental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                if (rental.getDaysRented() > 3)
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
                 break;
         }
         return thisAmount;
