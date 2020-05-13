0e86f9e90b1114619f059c744a26f6374c036b85
Tobias Bühler <tobias.buehler@t-online.de>
2020-05-13 12:11:27 +0200

    added Test to Rental

---
+++ src/tests/RentalTest.java
@@ -0,0 +1,44 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import classes.*;
+
+public class RentalTest {
+
+	private Rental rental;
+	private Movie movie;
+	
+	@Before
+	public void setUp(){
+		movie = new Movie("Janis stinkt",0);
+		rental = new Rental(movie,10);
+	}
+
+	@Test
+	public void getDaysRented() {
+		assertEquals(10,rental.getDaysRented());
+	}
+	
+	@Test
+	public void getMovie() 
+	{
+		assertEquals(movie,rental.getMovie());
+	}
+	
+	@Test
+	public void getFrequentRenterPoints() 
+	{
+		assertEquals(1,rental.getFrequentRenterPoints());
+	}
+	
+	@Test
+	public void getCharge() 
+	{
+		assertEquals(14.0,rental.getCharge(),0);
+	}
+
+}
