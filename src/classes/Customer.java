1cb46081eaa887b41646d3492596aaaf6cb28a61
Tobias Bühler <tobias.buehler@t-online.de>
2020-05-13 12:16:01 +0200

    changed vector rental into a list for less code

--- src/classes/Customer.java
+++ src/classes/Customer.java
@@ -6,14 +6,14 @@ public class Customer {
     
 	private String name;
     
-	private Vector<Rental> rentals = new Vector<Rental>();
+	private List<Rental> rentals = new ArrayList<>();
     
 	public Customer (String name){
         this.name = name;
     };
     
-    public void addRental(Rental arg) {
-        rentals.addElement(arg);
+    public void addRental(Rental rental) {
+        rentals.add(rental);
     }
     
     public String getName (){
@@ -25,25 +25,14 @@ public class Customer {
     	double totalAmount = 0;
         int frequentRenterPoints = 0;
         
-        Enumeration<Rental> enum_rentals = rentals.elements();	    
-        
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
-        while (enum_rentals.hasMoreElements()) {
-            
-            Rental rental = (Rental) enum_rentals.nextElement();
-
-            // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1) 
-                frequentRenterPoints ++;
-            //show figures for this rental
-            result += "\t" + rental.getMovie().getTitle()+ "\t" + "\t" + rental.getDaysRented() + "\t" + String.valueOf(rental.getCharge()) + "\n";
-            totalAmount += rental.getCharge();
+        for(Rental rental : rentals) 
+        {
+        	result += "\t" + rental.getMovie().getTitle()+ "\t" + "\t" + rental.getDaysRented() + "\t" + String.valueOf(rental.getCharge()) + "\n";
         }
-        //add footer lines
+        
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
